7a207336434b528ca355b9cfc8a6724e1fde2d67
Manuel Gutierrez <magutbar@gmail.com>
2023-03-17 13:56:06 +0100

    Primer commit Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// Modulo personalizado
+import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './ventas/ventas.module';
+import { SharedModule } from './shared/shared.module';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouterModule,
+    SharedModule,
+    VentasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
